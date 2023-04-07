90cc9d700b96c674270a8c259d62ae5561fce533
mviveslu <mviveslu@uoc.edu>
2023-04-07 19:32:42 +0200

    version to deliver

--- src/assets/scripts/swiper.js
+++ src/assets/scripts/swiper.js
@@ -1,14 +1,15 @@
-import Swiper from "swiper/swiper-bundle.esm.js";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
-import "swiper/swiper-bundle.css";
-var swiper = new Swiper(".mySwiper", {
-  autoplay: {
-    delay: 6000,
-  },
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+import Swiper from 'swiper/swiper-bundle.esm.js';
+  import 'swiper/swiper-bundle.min.css'
+  import 'swiper/swiper.min.css'
+  import 'swiper/swiper-bundle.css'
+
+  var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 6000,
+      },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
